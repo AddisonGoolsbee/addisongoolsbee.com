2a54de473204c6508707d56e8670857b7bd1fce3
Addison Goolsbee <addisongoolsbee@gmail.com>
2023-10-17 14:15:29 -0400

    Create solidjs template

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import type { Component } from 'solid-js';
+
+const App: Component = () => {
+  return (
+    <p class="text-4xl text-black text-center py-20">Hey there, how's it going?
+    I'm currently in the process of migrating my website to Solid.js, come check back soon for more updates!
+    You can reach me at addisongoolsbee@gmail.com</p>
+  );
+};
+
+export default App;
