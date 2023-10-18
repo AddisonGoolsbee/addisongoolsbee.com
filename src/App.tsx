0f1f861681093604817b205e668e06ea1dda4e6b
Addison Goolsbee <addisongoolsbee@gmail.com>
2023-10-18 01:11:39 -0400

    Update site text

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,7 @@ const App: Component = () => {
   return (
     <p class="text-4xl text-black text-center py-20">Hey there, how's it going?
     I'm currently in the process of migrating my website to Solid.js, come check back soon for more updates!
-    You can reach me at addisongoolsbee@gmail.com
-    
-    Oooooooh yeeeeaaaahhhhhh</p>
+    You can reach me at addisongoolsbee@gmail.com</p>
   );
 };
 
