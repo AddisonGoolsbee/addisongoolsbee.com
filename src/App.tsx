fa5e51e700cb480dd41b5b08998f6f7369dd868c
Addison Goolsbee <addisongoolsbee@gmail.com>
2023-10-18 00:47:17 -0400

    Create deploy script with custom domain automation

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,9 @@ const App: Component = () => {
   return (
     <p class="text-4xl text-black text-center py-20">Hey there, how's it going?
     I'm currently in the process of migrating my website to Solid.js, come check back soon for more updates!
-    You can reach me at addisongoolsbee@gmail.com</p>
+    You can reach me at addisongoolsbee@gmail.com
+    
+    Oooooooh yeeeeaaaahhhhhh</p>
   );
 };
 
