ab2d1b086edca0b12cf158cb50bf39f256c5f5bb
Addison Goolsbee <addisongoolsbee@gmail.com>
2023-10-30 19:53:45 -0400

    Implement profile image resizing and placement

--- README.md
+++ README.md
@@ -12,3 +12,15 @@ To test as a static site: `npm run build && npm run serve`
 ## Deployment
 
 Build and deploy the website with: `npm run deploy`
+
+<div class="w-1/2 h-full">{/* The rest of your content for the right half can go here */}</div>
+      <div class="flex flex-col justify-center h-[calc(100%-4rem)] pl-1/2 bg-gray-300">
+        <div class="w-2/3 p-8">
+          <h1 class="text-5xl mb-4">Hey, I'm Addison!</h1>
+          <p class="text-xl mb-4">Welcome to my website. I'm a college student interested in software engineering and systems design.</p>
+          <p class="text-xl">Feel free to reach out to me:</p>
+          <a href="mailto:addisongoolsbee@gmail.com" class="text-xl text-blue-500">
+            addisongoolsbee@gmail.com
+          </a>
+        </div>
+      </div>
--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,19 @@
-import type { Component } from 'solid-js';
+import type { Component } from "solid-js";
 
 const App: Component = () => {
   return (
-    <div class="h-screen overflow-hidden bg-gray-800">
-      <div class="h-16 bg-black text-white flex items-center px-4">
-        Top Bar
-      </div>
-      <div class="flex h-[calc(100%-4rem)]">
-        <div class="w-1/3 flex justify-center items-center">
-          <img src="/images/profile.png" alt="Addison" class="object-cover w-full h-3/4"/>
-        </div>
-        <div class="w-2/3 p-8 text-white">
-          <h1 class="text-5xl mb-4">Hey, I'm Addison!</h1>
-          <p class="text-xl mb-4">
-            Welcome to my website. I'm a college student interested in software engineering and systems design.
-          </p>
-          <p class="text-xl">Feel free to reach out to me:</p>
-          <a href="mailto:addisongoolsbee@gmail.com" class="text-xl text-blue-500">
-            addisongoolsbee@gmail.com
-          </a>
-        </div>
-      </div>
-    </div>
+<div class="h-screen overflow-hidden relative bg-gray-800">
+  <div class="h-16 bg-black text-white flex items-center px-4 z-10">Top Bar</div>
+  <div class="absolute w-full h-5/6 bottom-0 flex items-end" style={{ left: "25%", transform: "translateX(-50%)" }}>
+    <img src="/images/profile.png" alt="Addison" class="w-full h-auto object-contain max-h-full" />
+  </div>
+</div>
+
+
+  
+
   );
 };
 
 export default App;
+
