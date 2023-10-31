bfb8d64a2dedc5c0945f743732cb95b76148ac7a
Addison Goolsbee <addisongoolsbee@gmail.com>
2023-10-31 15:49:35 -0400

    Add background dots animation

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
 import type { Component } from "solid-js";
+import Particles from "./Particles";
 
 const App: Component = () => {
   return (
     <div class="h-screen overflow-hidden relative bg-gray-800">
+      <Particles />
       <div class="h-16 text-white flex items-center px-6 z-10">Addison Goolsbee</div>
       <div class="absolute w-full h-9/10 bottom-0 flex items-end" style={{ left: "22%", transform: "translateX(-50%)" }}>
         <img src="/images/profile.png" alt="Addison" class="w-full h-auto object-contain max-h-full" />
       </div>
       <div class="absolute top-0 right-0 w-2/3 h-full p-5p">
-        <div class=" h-full bg-white bg-opacity-50 z-10 p-8 ml-7p">
+        <div class=" h-full bg-white bg-opacity-50 z-10 p-8 ml-7p rounded-sm shadow-2xl">
           <p class="text-black">
             Hey there! I'm currently in the process of building my new website, come check back for frequent updates!
             My email is addisongoolsbee@gmail.com if you want to contact me
---
+++ src/Particles.tsx
@@ -0,0 +1,92 @@
+import { onCleanup, onMount, createSignal, createEffect } from "solid-js";
+import type { Component } from "solid-js";
+
+interface Particle {
+  x: number;
+  y: number;
+  maxSize: number;
+  currentSize: number;
+  increasing: boolean;
+  speedX: number;
+  speedY: number;
+}
+
+const Particles: Component = () => {
+  const SPEED = 1;
+  const SIZE = 7;
+  const GROWTH = 0.05;
+  const MAX_AMOUNT = 120;
+
+  let currentAmount = 0;
+  let canvasRef: HTMLCanvasElement | undefined;
+
+  const initParticles = () => {
+    const canvas = canvasRef;
+    if (!canvas) return;
+
+    const ctx = canvas.getContext("2d");
+    if (!ctx) return;
+
+    const particles: Particle[] = [];
+    let canvasSize = { width: window.innerWidth, height: window.innerHeight };
+    let animationFrameId: number;
+
+    canvas.width = canvasSize.width;
+    canvas.height = canvasSize.height;
+
+    const createParticle = () => {
+      currentAmount++;
+      particles.push({
+        x: Math.random() * canvas.width,
+        y: Math.random() * canvas.height,
+        maxSize: Math.random() * SIZE + 1,
+        currentSize: 0,
+        increasing: true,
+        speedX: Math.random() * SPEED - SPEED / 2,
+        speedY: Math.random() * SPEED - SPEED / 2,
+      });
+    };
+
+    const animateParticles = () => {
+      ctx.clearRect(0, 0, canvas.width, canvas.height);
+      for (let i = 0; i < particles.length; i++) {
+        let p = particles[i];
+        ctx.beginPath();
+        ctx.arc(p.x, p.y, p.currentSize, 0, Math.PI * 2);
+        ctx.fillStyle = "#8ff";
+        ctx.fill();
+        p.x += p.speedX;
+        p.y += p.speedY;
+        if (p.increasing) {
+          p.currentSize += GROWTH;
+          if (p.currentSize >= p.maxSize) p.increasing = false;
+        } else {
+          p.currentSize -= GROWTH;
+          if (p.currentSize <= 0.2) {
+            particles.splice(i, 1);
+            i--;
+            currentAmount--;
+          }
+        }
+      }
+      if (currentAmount < MAX_AMOUNT) {
+        createParticle();
+      }
+      animationFrameId = requestAnimationFrame(animateParticles);
+    };
+
+    animateParticles();
+
+    onCleanup(() => {
+      cancelAnimationFrame(animationFrameId);
+    });
+  };
+
+  onMount(() => {
+    initParticles();
+  });
+
+  return <canvas ref={canvasRef} class="fixed top-0 left-0 w-full h-full z-0"></canvas>;
+};
+
+export default Particles;
