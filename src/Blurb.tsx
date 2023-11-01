14bc0710d4136be4eee4a3294113277d35992848
Addison Goolsbee <addisongoolsbee@gmail.com>
2023-10-31 22:06:43 -0400

    Fix particle resize glitch

--- src/Blurb.tsx
+++ src/Blurb.tsx
@@ -5,7 +5,7 @@ import { FiMail, FiFileText } from "solid-icons/fi";
 const Blurb: Component = () => {
   return (
     <div class="absolute top-0 right-0 w-2/3 h-full p-7p pl-10p flex flex-col">
-      <div class="text-black bg-white bg-opacity-60 z-10 p-8 rounded-sm shadow-2xl flex-grow">
+      <div class="text-black bg-white bg-opacity-70 z-10 p-8 rounded-sm shadow-2xl flex-grow">
         <p class="text-3xl font-extralight">
           My name is <span class="text-teal-800 font-medium">Addison Goolsbee</span>
         </p>
@@ -13,7 +13,7 @@ const Blurb: Component = () => {
         <p> Hey there! I'm a junior at Yale University studying computer science. Welcome to my website! </p>
         <br />
         <p>
-          I'm a fullstack developer with a particular interest in system design/development. I love building things—useful applications, extremely useless applications, video games, electronic devices, water jug art installations, etc.&nbsp;
+          I'm a fullstack developer with a particular interest in system design/development. I love building things—useful applications, extremely useless applications, video games, physical devices, water jug art installations, etc.&nbsp;
           <a href="https://github.com/addisongoolsbee" target="_blank" class="link">
             Check out some of my projects
           </a>
@@ -24,13 +24,13 @@ const Blurb: Component = () => {
           <a href="https://github.com/addisongoolsbee/addisongoolsbee.com" class="link">
             The code is public
           </a>
-          .&nbsp;You can look at my previous 1-hour template-based website&nbsp;
+          .&nbsp;You can look at my previous template-based (lame) website&nbsp;
           <a href="/images/initialWebsite.png" class="link">
             here
           </a>
         </p>
       </div>
-      <div class="w-full bg-teal-800 p-5 flex justify-center space-x-8">
+      <div class="w-full bg-teal-800 bg-opacity-90 p-5 flex justify-center space-x-7p">
         <a href="https://www.linkedin.com/in/addisongoolsbee" target="_blank" class="text-white text-4xl">
           <FaBrandsLinkedin />
         </a>
