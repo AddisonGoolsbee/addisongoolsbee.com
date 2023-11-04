752ce1ce7cf4a2f9c08c8cf5c382f2f7ef988fa8
Addison Goolsbee <addisongoolsbee@gmail.com>
2023-11-03 21:15:21 -0400

    Implement changelog panel but not changelog content

--- README.md
+++ README.md
@@ -19,23 +19,28 @@ Build and deploy the website with: `npm run deploy`
 **Version 1.0**
 - add more content to blurb
 - changelog in header
-- phone/narrow layout
 - something that reveals the can gif
 
 **Version 1.1**
 - create color palette
 - initial page MEET ADDISON followed by fake-scroll scrolldown
 - previous versions live demo
+- custom font(s)
+- more button hover effects
 
 **Version 1.2**
 - PROJECTS PAGE
 - secrets
+
+**Bugs To Fix**
+- https is not longer required...?
 
 **Future Additions**
 - add some more decorations to the screen
 - auto-update with new github projects
 - mouse effects
 - more secrets
+- QR code page
 
 
 #### Inspirations
--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
-import { onMount, type Component, onCleanup, createSignal, Show } from "solid-js";
+import { onMount, type Component, onCleanup, createSignal, Show, createEffect } from "solid-js";
 import Particles from "./Particles";
 import Blurb from "./Blurb";
+import Changelog from "./Changelog";
 
 const App: Component = () => {
   let imgRef;
+  
   const [topPoint, setTopPoint] = createSignal(window.innerHeight);
   const [imageLoaded, setImageLoaded] = createSignal(false);
+  const [changelogVisible, setChangelogVisible] = createSignal(false);
+  
 
   const updateTopPoint = () => {
     if (imgRef) {
@@ -13,11 +17,11 @@ const App: Component = () => {
       if (window.innerWidth >= 640) {
         setTopPoint(0);
       } else {
-        console.log(rect);
         setTopPoint(rect.height);
       }
     }
   };
+
   const onImageLoad = () => {
     updateTopPoint();
     setImageLoaded(true);
@@ -32,11 +36,21 @@ const App: Component = () => {
     window.removeEventListener("resize", updateTopPoint);
   });
 
+  const toggleChangelog = () => {
+    setChangelogVisible(!changelogVisible());
+  };
+
+
   return (
     <div class="h-screen overflow-hidden relative bg-gray-800">
       <Particles />
-      <div class="h-16 text-white flex flex-row items-end z-10">
+      <div class="h-16 text-white flex flex-row justify-end z-10 px-4 space-x-4">
         <img src="/images/whiteLogo.svg" alt="logo" class="absolute top-0 left-0 ml-4 mt-2 h-auto w-10 sm:w-12 cursor-pointer animate-logo" draggable="false" onClick={() => (window.location.href = "/")} />
+        <div class="flex flex-row items-center space-x-4">
+          <button class="text-white cursor-pointer z-20 p-2" onClick={toggleChangelog}>
+            Changelog
+          </button>
+        </div>
       </div>
       <div class="absolute w-5/6 h-9/10 bottom-0 flex items-end left-1/2 transform -translate-x-1/2 sm:left-22p">
         <img src="/images/profile.png" alt="Addison" class="w-full h-auto object-contain max-h-full animate-slide-up" draggable="false" ref={imgRef} onLoad={onImageLoad} />
@@ -44,6 +58,7 @@ const App: Component = () => {
       <Show when={imageLoaded()}>
         <Blurb imgTop={topPoint()} />
       </Show>
+      <Changelog changelogVisible={changelogVisible} setChangelogVisible={setChangelogVisible} toggleChangelog={toggleChangelog}/>
     </div>
   );
 };
