be3338cfc5e5dca6c18e0b280905bac4d5be6738
Addison Goolsbee <addisongoolsbee@gmail.com>
2023-11-25 00:33:34 -0600

    Updates

---
+++ assets/canonical-38d2fe50.js
@@ -0,0 +1,1 @@
+import{w as a,b as t}from"./index-7a1d08fe.js";function e(){const o=a();t(()=>{const n=document.getElementById("canonical-link");n&&(n.href=`https://addisongoolsbee.com${o.pathname}`)})}export{e as u};
