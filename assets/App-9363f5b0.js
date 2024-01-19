b5e1cfca75c0a71e16acd7a1fe3fd6031a013544
Addison Goolsbee <addisongoolsbee@gmail.com>
2024-01-19 18:28:54 -0500

    Updates

---
+++ assets/App-9363f5b0.js
@@ -0,0 +1,1 @@
+import{t as e}from"./index-c47b525c.js";import{u as t}from"./canonical-4d182fa1.js";const o=e(`<p class="text-4xl text-black text-center py-20">Hey there, how's it going? I'm currently in the process of migrating my website to Solid.js, come check back soon for more updates! You can reach me at addisongoolsbee@gmail.com`),r=()=>(t(),o());export{r as default};
