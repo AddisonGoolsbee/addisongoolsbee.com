4b8d9047ea693be0a0cf935b2d51fc4e87880fdb
Addison Goolsbee <addisongoolsbee@gmail.com>
2024-11-10 22:39:21 -0500

    Updates

---
+++ assets/canonical-929931fa.js
@@ -0,0 +1,1 @@
+import{n as a,b as t}from"./index-360b6fcf.js";function e(){const o=a();t(()=>{const n=document.getElementById("canonical-link");n&&(n.href=`https://addisongoolsbee.com${o.pathname}`)})}export{e as u};
---
+++ assets/temp-9d0b585f.js
@@ -0,0 +1,1 @@
+import{P as r}from"./Party-2417d683.js";import{h as t}from"./index-360b6fcf.js";const e=()=>t(r,{});export{e as default};
