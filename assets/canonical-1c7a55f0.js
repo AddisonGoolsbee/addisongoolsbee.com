996e17b05b5818a96fd9db2971708de365beb250
Addison Goolsbee <addisongoolsbee@gmail.com>
2025-03-08 15:51:53 -0500

    Updates

---
+++ assets/canonical-1c7a55f0.js
@@ -0,0 +1,1 @@
+import{k as a,b as t}from"./index-fcec6000.js";function e(){const o=a();t(()=>{const n=document.getElementById("canonical-link");n&&(n.href=`https://addisongoolsbee.com${o.pathname}`)})}export{e as u};
