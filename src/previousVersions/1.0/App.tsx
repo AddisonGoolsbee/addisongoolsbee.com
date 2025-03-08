ff538d1d144bf7f0de84535390b4c5791a46de1a
Addison Goolsbee <addisongoolsbee@gmail.com>
2025-03-08 15:37:17 -0500

    Attempt again to fix indexing problem

--- src/previousVersions/1.0/App.tsx
+++ src/previousVersions/1.0/App.tsx
@@ -1,20 +1,11 @@
-import { Routes, Route } from "@solidjs/router";
 import { lazy } from "solid-js";
 
 const Home = lazy(() => import("./pages/Home"));
-const v1_0 = lazy(() => import("../1.0/App"));
-const v0_2 = lazy(() => import("../0.2/App"));
-const v0_1 = lazy(() => import("../0.1/App"));
 
 export default function App() {
   return (
     <>
-      <Routes>
-        <Route path="/" component={Home} />
-        <Route path="/v1_0" component={v1_0} />
-        <Route path="/v0_2" component={v0_2} />
-        <Route path="/v0_1" component={v0_1} />
-      </Routes>
+      <Home />
     </>
   );
 }
