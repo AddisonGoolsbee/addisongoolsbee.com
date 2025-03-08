0f3fbe1c381f41d40dbd2df23b0ea5100bd7d406
Addison Goolsbee <addisongoolsbee@gmail.com>
2025-03-08 17:39:09 -0500

    Add confetti to party mode

--- src/components/Party.tsx
+++ src/components/Party.tsx
@@ -1,11 +1,14 @@
-import { createEffect, onCleanup } from 'solid-js';
+import { createEffect, onCleanup } from "solid-js";
+import Confetti from "./Confetti";
 
 const Party = () => {
   const isMobile = window.innerWidth < 640;
 
   createEffect(() => {
     const spawnCircles = () => {
-      const container = document.querySelector('#party-container') as HTMLElement;
+      const container = document.querySelector(
+        "#party-container"
+      ) as HTMLElement;
       if (!container) return;
 
       const numRows = isMobile ? 7 : 11;
@@ -15,7 +18,7 @@ const Party = () => {
       const maxRowFactor = Math.pow(centerRow, 2);
       const speed = isMobile ? 5 : 7;
       const sphereRadius = isMobile ? 25 : 20;
-      const sphereHeightScale = isMobile ? 0.5 : 1
+      const sphereHeightScale = isMobile ? 0.5 : 1;
 
       const getRainbowColor = (index: number, total: number): string => {
         const hue = (index / total) * 360;
@@ -28,41 +31,56 @@ const Party = () => {
       for (let col = 0; col < numColumns; col++) {
         for (let row = 0; row < numRows; row++) {
           const animationDelay = (col / numColumns) * speed;
-          const rowFactor = (Math.pow(row - centerRow, 2) * sphereRadius) / maxRowFactor;
+          const rowFactor =
+            (Math.pow(row - centerRow, 2) * sphereRadius) / maxRowFactor;
 
-          const circle = document.createElement('div');
+          const circle = document.createElement("div");
           circle.style.width = `${ballRadius}px`;
           circle.style.height = `${ballRadius}px`;
           circle.classList.add(
-            'absolute',
-            'rounded-full',
-            'opacity-0',
-            'transition-opacity',
-            'duration-300',
-            'ease-in-out'
+            "absolute",
+            "rounded-full",
+            "opacity-0",
+            "transition-opacity",
+            "duration-300",
+            "ease-in-out"
           );
 
-          circle.style.backgroundColor = getRainbowColor(col + numColumns, numColumns);
-          circle.style.top = `calc(${((row * rowHeight) * sphereHeightScale) + (100 - sphereHeightScale * 100)}dvh - ${ballRadius * ((row + (row > 0 ? 1 : 0)) / numRows)
-            }px)`;
+          circle.style.backgroundColor = getRainbowColor(
+            col + numColumns,
+            numColumns
+          );
+          circle.style.top = `calc(${
+            row * rowHeight * sphereHeightScale +
+            (100 - sphereHeightScale * 100)
+          }dvh - ${ballRadius * ((row + (row > 0 ? 1 : 0)) / numRows)}px)`;
           circle.style.animationDelay = `${animationDelay}s`;
           circle.style.animationDuration = `${speed}s`;
-          circle.style.animationIterationCount = 'infinite';
+          circle.style.animationIterationCount = "infinite";
 
           // Create unique keyframes if not already created
           const keyframeName = `move-row-${row}-${col}`;
           const existingRule = [...document.styleSheets[0].cssRules].some(
-            (rule) => rule instanceof CSSKeyframesRule && rule.name === keyframeName
+            (rule) =>
+              rule instanceof CSSKeyframesRule && rule.name === keyframeName
           );
 
           if (!existingRule) {
             const keyframes = `
               @keyframes ${keyframeName} {
-                0% { left: calc(50vw - ${ballRadius / 2}px); z-index: 1000; animation-timing-function: ease-out; opacity: 1; }
-                25% { left: calc(${100 - rowFactor}vw - ${ballRadius}px); z-index: 500; animation-timing-function: ease-in; }
-                50% { left: calc(50vw - ${ballRadius / 2}px); z-index: 1; animation-timing-function: ease-out; }
+                0% { left: calc(50vw - ${
+                  ballRadius / 2
+                }px); z-index: 1000; animation-timing-function: ease-out; opacity: 1; }
+                25% { left: calc(${
+                  100 - rowFactor
+                }vw - ${ballRadius}px); z-index: 500; animation-timing-function: ease-in; }
+                50% { left: calc(50vw - ${
+                  ballRadius / 2
+                }px); z-index: 1; animation-timing-function: ease-out; }
                 75% { left: calc(${rowFactor}vw); z-index: 500; animation-timing-function: ease-in; }
-                100% { left: calc(50vw - ${ballRadius / 2}px); z-index: 1000; animation-timing-function: ease-out; opacity: 1; }
+                100% { left: calc(50vw - ${
+                  ballRadius / 2
+                }px); z-index: 1000; animation-timing-function: ease-out; opacity: 1; }
               }`;
             (document.styleSheets[0] as CSSStyleSheet).insertRule(
               keyframes,
@@ -82,13 +100,18 @@ const Party = () => {
     // Cleanup function to remove circles and clear timeout when unmounting
     onCleanup(() => {
       clearTimeout(spawnTimeout);
-      const container = document.querySelector('#party-container') as HTMLElement;
-      if (container) container.innerHTML = '';
+      const container = document.querySelector(
+        "#party-container"
+      ) as HTMLElement;
+      if (container) container.innerHTML = "";
     });
   });
 
   return (
-    <div id="party-container" class="absolute inset-0 overflow-hidden"></div>
+    <>
+      <div id="party-container" class="absolute inset-0 overflow-hidden"></div>
+      <Confetti />
+    </>
   );
 };
 
