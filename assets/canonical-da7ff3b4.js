3c2d9854fe11515e0c3aded65668dbaa54f1bcbf
Addison Goolsbee <addisongoolsbee@gmail.com>
2025-04-27 14:37:12 -0400

    Updates

--- assets/canonical-da7ff3b4.js
+++
@@ -1,1 +0,0 @@
-import{k as a,b as t}from"./index-4dbda686.js";function e(){const o=a();t(()=>{const n=document.getElementById("canonical-link");n&&(n.href=`https://addisongoolsbee.com${o.pathname}`)})}export{e as u};
