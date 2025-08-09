14a967e02094aa47772c918323917c7a00d1e765
Addison Goolsbee <addisongoolsbee@gmail.com>
2025-08-09 10:37:36 +0200

    Update blurb

--- src/components/Blurb.tsx
+++ src/components/Blurb.tsx
@@ -28,7 +28,7 @@ type Props = {
 };
 
 export const defaultBlurbStart =
-  "Hey there! I'm a soon-to-be software engineer living in Seattle. I'm a full stack developer with an obsession for system design and making stuff—sometimes useful, sometimes gloriously pointless. I've built web apps, video games, physical gadgets, five gallon water jug 'art installations', among others.";
+  "Hey there! I'm a soon-to-be software engineer living in Seattle. I'm a full stack developer with an obsession for system design and making stuff—sometimes useful, sometimes gloriously pointless. I've built web apps, video games, physical gadgets, five gallon water jug 'art installations', and more.";
 
 const Blurb: Component<Props> = (props) => {
   const [windowWidth, setWindowWidth] = createSignal(window.innerWidth);
