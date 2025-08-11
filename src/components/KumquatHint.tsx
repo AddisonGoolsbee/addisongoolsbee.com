d22480ca73597605bda03949486cf0ddcd4f6c67
Addison Goolsbee <addisongoolsbee@gmail.com>
2025-08-11 12:03:08 +0100

    Make kumquat hint fade in slowly

--- src/components/KumquatHint.tsx
+++ src/components/KumquatHint.tsx
@@ -16,15 +16,26 @@ const Space = () => {
 
 const KumquatHint: Component = () => {
   return (
-    <div class="fixed left-0 top-1/2 -translate-y-1/2 text-white/50 z-[1000] flex flex-col gap-1">
+    <div class="fixed left-0 top-1/2 -translate-y-1/2 text-white/50 z-[1000] flex flex-col gap-1 opacity-0 animate-fadein5s">
+      <style>
+      {`
+        @keyframes fadein5s {
+        from { opacity: 0; }
+        to { opacity: 1; }
+        }
+        .animate-fadein5s {
+        animation: fadein5s 5s ease-in forwards;
+        }
+      `}
+      </style>
       {Array.from(code).map((char, idx) => {
-        if (char === ".") {
-          return <Dot />;
-        }
-        if (char === "_") {
-          return <Dash />;
-        }
-        return <Space />;
+      if (char === ".") {
+        return <Dot />;
+      }
+      if (char === "_") {
+        return <Dash />;
+      }
+      return <Space />;
       })}
     </div>
   );
