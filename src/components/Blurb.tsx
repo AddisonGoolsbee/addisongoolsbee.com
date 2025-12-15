74632955d68be0cb3b0a10d763ea2d0a8052ecc9
Addison Goolsbee <addisongoolsbee@gmail.com>
2025-12-14 22:26:29 -0800

    Minor improvements

--- src/components/Blurb.tsx
+++ src/components/Blurb.tsx
@@ -197,7 +197,7 @@ const Blurb: Component<Props> = (props) => {
       </p>
       <p class="mb-4">
         Most secrets are unlocked with a password—try typing 'bicycle man'. If you're on a phone, triple tap me to
-        summon the keyboard, but beware, some puzzles can only be solved on a full-sized screen.
+        summon the keyboard, but beware, some puzzles can only be solved on a computer.
       </p>
       {/* <p class="mb-4">
         Some secrets require a bit of technical know-how. Hover over a tech-hint
@@ -223,6 +223,9 @@ const Blurb: Component<Props> = (props) => {
           <span class="font-bold">Other Websites:</span> It's all here unless a hint says otherwise.
         </li>
       </ul>
+      <p class='mb-4'>
+        Reset the state of the site with 'reset'. I recommend having your sound on.
+      </p>
       <p>
         Maybe there are some secrets hidden in this very page...&nbsp;
         <span class="text-transparent select-text secret-select">garbfpamfrlms</span>
